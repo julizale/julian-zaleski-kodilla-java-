1e225aa9d852157f6b4f73d9e3073761277739f8
Julian Zaleski <julizale@gmail.com>
2022-11-03 19:55:22 +0100

    17.2 Added com.kodilla.hibernate.task

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -1,4 +1,4 @@
-package com.kodilla.kodilla.hibernate;
+package com.kodilla.hibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KodillaHibernateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KodillaHibernateApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateApplication.class, args);
+    }
 }
