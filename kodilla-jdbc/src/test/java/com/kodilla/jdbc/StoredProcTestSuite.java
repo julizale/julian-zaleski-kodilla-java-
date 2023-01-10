ee6d1478e81ac9ad5b88f5b3ef7c70b71aa05697
Julian Zaleski <julizale@gmail.com>
2023-01-10 15:11:29 +0100

    30.3 SQL exercise

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -36,4 +36,37 @@ public class StoredProcTestSuite {
         statement2.close();
     }
 
+    @Test
+    void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        Statement statement = dbManager.getConnection().createStatement();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=0";
+
+        statement.executeUpdate(sqlUpdate);
+
+        String sqlSelectExpectedBestsellers = "SELECT COUNT(*) AS HOW_MANY FROM (SELECT NULL FROM RENTS" +
+                " GROUP BY BOOK_ID HAVING COUNT(*) > 2) T1";
+        ResultSet expectedBestsellers = statement.executeQuery(sqlSelectExpectedBestsellers);
+        int howManyExpected = -1;
+        if (expectedBestsellers.next()) {
+            howManyExpected = expectedBestsellers.getInt("HOW_MANY");
+        }
+
+        //When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+        String sqlSelectActualBestsellers = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER = 1";
+        ResultSet actualBestsellers = statement.executeQuery(sqlSelectActualBestsellers);
+        int howManyActual = -1;
+        if (actualBestsellers.next()) {
+            howManyActual = actualBestsellers.getInt("HOW_MANY");
+        }
+
+        //Then
+        assertEquals(howManyExpected, howManyActual);
+        expectedBestsellers.close();
+        actualBestsellers.close();
+        statement.close();
+    }
 }
